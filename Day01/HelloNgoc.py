ffede058421c6396ab9b2b26bf94b0117018d9ba
minhngoc <ngoc.nm.hvhc@gmail.com>
2021-01-25 02:53:41 +0700

    Python crash course pg01-23

---
+++ Day01/HelloNgoc.py
@@ -0,0 +1,11 @@
+def firstFunc():
+    #this is func for Ngoc  
+    x = "Ngoc xinh dep"
+    print(x)
+    
+def secondFunc() :
+    y = "Nam cu teo"
+    print(y)    
+   
+#firstFunc()
+secondFunc()
