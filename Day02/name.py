2d1587d2cf82ee49f212eae6b31db8910a776f66
minhngoc <ngoc.nm.hvhc@gmail.com>
2021-01-25 22:05:30 +0700

    Print String in python

---
+++ Day02/name.py
@@ -0,0 +1,24 @@
+def firstFunc(): 
+    name = "ada lovelace"
+    print(name.title())
+    print(name.upper())
+    print(name.lower())
+    
+def secondFunc():
+    first_name = "Nguyen"
+    last_name = "Minh Ngoc"
+    full_name = first_name + " " + last_name
+    print("Hello, " + full_name.title() + "!")  
+def twothFunc():
+    print("Hello,\n\tNguyen\n\tMinh Ngoc")
+
+def thirdFunc():
+    user_name = "Gemmy"
+    message = "Hello " + user_name.upper() + ", how are you today?"
+    print (message)
+    
+
+def fourthFunc():
+    print("Ngoc once said, \"Your wife always right\" ")
+    
+fourthFunc()
