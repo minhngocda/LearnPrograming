a5ab1f0047108774b4ec4c8807b4df2459f3d3d4
minhngoc <ngoc.nm.hvhc@gmail.com>
2021-01-27 23:22:52 +0700

    Add a massage from list

--- Day04/family.py
+++ Day04/family.py
@@ -7,3 +7,7 @@ print(family[0])
 print(family[1].title())
 # to print -3 element with upper menthod, it is the third when return from the first item  
 print(family[-3].upper())
+#to print message from family
+message= "I love my " + family[1].title() + "."
+print(message)
+
