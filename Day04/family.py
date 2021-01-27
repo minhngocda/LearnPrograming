3601d21f38e3cde9ce8c5724d41395a904dc0ada
minhngoc <ngoc.nm.hvhc@gmail.com>
2021-01-27 16:50:49 +0700

    print from list

---
+++ Day04/family.py
@@ -0,0 +1,9 @@
+family = ['husband', 'wife', 'kids']
+# to print list family
+print(family)
+# to print first element in the list
+print(family[0])
+# to print second element with title menthod
+print(family[1].title())
+# to print -3 element with upper menthod, it is the third when return from the first item  
+print(family[-3].upper())
