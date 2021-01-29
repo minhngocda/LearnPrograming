95696017b57eb277d87e3c60295d25ae1ec409ca
minhngoc <ngoc.nm.hvhc@gmail.com>
2021-01-29 15:52:38 +0700

    update

--- Day03/birthday.py
+++ Day03/birthday.py
@@ -7,7 +7,7 @@ if age < 0:
 age = str(age)
 if age[-1] == "1" and age != "11" and age != "111" :
     print("Happy " + age + "st Birthday!")
-elif age[-1] == "2":
+elif age[-1] == "2" and age!= "12":
     print("Happy " + age + "nd Birthday!")     
 elif age[-1] == "3":
     print("Happy " + age + "rd Birthday!")   
