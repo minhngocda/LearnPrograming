18a43517b398b3045144ddff3f0a4380cec1e8fa
minhngoc <ngoc.nm.hvhc@gmail.com>
2021-01-29 17:45:57 +0700

    Day06

--- Day06/guest list.py
+++ Day06/guest list.py
@@ -1,3 +1,7 @@
 guest_list=['Anna', 'Ben', 'Cynthia','Diana','Elon']
+guest_list.append('Fiona')
+guest_list.insert(2,'Gene')
+pop_guest = guest_list.pop(4)
 for x in range(len(guest_list)):
     print("Hello " + guest_list[x] + ", I want you to join my party at 8p.m today")
+print("Sorry that you can not come " + pop_guest)
