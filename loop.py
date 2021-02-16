655b46459ceec9ae279ede02c69570f7633146ea
minhngoc <ngoc.nm.hvhc@gmail.com>
2021-02-16 15:45:11 +0700

    loops

--- loop.py
+++ loop.py
@@ -1,3 +1,5 @@
 magicians = ['Nam' , 'Ngoc', 'Min' ]
 for a in magicians:
-    print(a)
+    print(a.title() + ", that awesome!")
+    print("I can't wait to see your next show, " + a.title() +".\n")
+print("Thank you, everyone. That was the best show!")
