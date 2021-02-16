cbff9d06c3843e1d9d0c41fe5ff9c960fe659348
minhngoc <ngoc.nm.hvhc@gmail.com>
2021-02-16 16:11:14 +0700

    loops

--- loop.py
+++ loop.py
@@ -1,5 +1,14 @@
-magicians = ['Nam' , 'Ngoc', 'Min' ]
-for a in magicians:
-    print(a.title() + ", that awesome!")
-    print("I can't wait to see your next show, " + a.title() +".\n")
-print("Thank you, everyone. That was the best show!")
+def FirstFunc():
+    magicians = ['Nam' , 'Ngoc', 'Min' ]
+    for a in magicians:
+        print(a.title() + ", that awesome!")
+        print("I can't wait to see your next show, " + a.title())
+    print("Thank you, everyone. That was the best show!" +".\n")
+def SecondFunc():
+    pizzas=["pepperoni" , "cheese" , "beef-pipeapple"]
+    for piz in pizzas:
+        print("I love " + piz.title() + " pizza.")
+    print("I really love pizza")
+
+SecondFunc()
+
