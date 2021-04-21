f7a1006fc288a78ab55de68556eb406c379a5807
minhngoc <ngoc.nm.hvhc@gmail.com>
2021-04-21 20:55:57 +0700

    Homework If/elif/else

---
+++ Day09/page88.py
@@ -0,0 +1,33 @@
+alien_color = 'green'
+if alien_color == 'green':
+    print("You earned 5 points.")
+elif alien_color == 'yellow':
+    print("You earned 10 points.")
+elif alien_color == 'red': 
+    print("You earned 15 points.")
+    
+x = 65
+if x<2:
+    print("You are a baby")
+elif x >=2 and x<4:
+    print("You are a toddler.")
+elif x>=4 and x<13:
+    print("You are a kid.")    
+elif x>=13 and x<20:
+    print("You are a teenager")
+elif x>=20 and x<65:
+    print("You are an adult.")
+else:
+    print ("You are an elder.")
+
+favourite_fruit=['mango' , 'banana' , 'apple']
+if 'mango' in favourite_fruit:
+    print("You really like mango")
+if 'banana' in favourite_fruit:
+    print("You really like banana")
+if 'apple' in favourite_fruit:
+    print("You really like apple")
+if 'papaya' in favourite_fruit:
+    print("You really like papaya")
+if 'physalis' in favourite_fruit:
+    print("You really like physalis")    
